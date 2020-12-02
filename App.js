44db02218d94be13f5e24f92881c6ac18a689602
Samuel010101 <45717648+Samuel010101@users.noreply.github.com>
2020-12-02 17:24:07 -0400

    Add files via upload
    More files from my react practices

--- App.js
+++ App.js
@@ -1,29 +1,26 @@
 import React from 'react';
-import unsplash from '../api/unsplash';
-import SearchBar from './SearchBar';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends React.Component {
-    state = { images: [] }; 
-
-     onSearchSumit = async term => {
-        // Este bloque de codigo es el api o funcionalidad que me permite hacer request a un servidor externo para extraer imagenes
-       const response = await unsplash.get('/search/photos', {
-           params: { query: term },
-            
-            }); /*.then(response => {
-                console.log(response.data.results);
-                }); */
-        this.setState({ images: response.data.results });
-    }
-
-    render() {
-    return (
-            <div className="ui container" style={{ marginTop: '10px' }}>
-                <SearchBar onSubmit={this.onSearchSumit} />
-                found: {this.state.images.length} images
-            </div>
-        )
-    } 
-};
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
--- index.js
+++ index.js
@@ -1,7 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import 'semantic-ui-css/semantic.min.css';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import AprovalCard from './ApprovalCard';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const App = () => {
+    return (
+        <div className="ui container comments" >
+            <AprovalCard>
+                <div>
+                    <h4>
+                    Are you sure yo want to do this?
+                    </h4>
+                </div>
+            </AprovalCard>
 
+            <AprovalCard>
+                <CommentDetail author="Sam" 
+                    timeAgo="Yesterday at 11:50PM" 
+                    content="The blog is fantasty" 
+                    avatar={faker.image.avatar()}
+                />
+            </AprovalCard>
+
+            <AprovalCard>
+            <CommentDetail author="Juan" 
+                timeAgo="Today at 3:00AM" 
+                content="Great code practice" 
+                avatar={faker.image.avatar()}
+            />
+            </AprovalCard>
+
+            <AprovalCard>
+            <CommentDetail author="Ivan" 
+                timeAgo="Today at 7AM" 
+                content="I like the subject" 
+                avatar={faker.image.avatar()}
+            />
+            </AprovalCard>
+            
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'))
