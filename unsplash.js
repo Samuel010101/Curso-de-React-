9d79437acacc1dc468a7ea6d5fee78a3bd3abd38
Samuel010101 <45717648+Samuel010101@users.noreply.github.com>
2020-12-03 22:37:24 -0400

    Delete unsplash.js

--- unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({ 
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization: 
-            'Client-ID OuzAGb6ky3RRFgx2HoDdWs1S1SYk2u5xaI17UzEPUZo'
-    }       
-});
